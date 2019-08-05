59a227f2a13a40546d3f6d9efbc7c7b31a2b258d
Kevin Tou <toukevin@gmail.com>
2019-08-05 13:47:39 -0700

    Added todo submission and clear

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Todo = props => {
   return (
-    <>
+    <div
+      className={props.todo.completed ? 'completed' : ''}
+      onClick={() => props.toggleCompleted(props.todo.id)}>
       <p>{props.todo.task}</p>
-    </>
+    </div>
   );
 };
 
